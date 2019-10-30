58da34cabcf9bfb3fbb2aec9871c6a9248ecf269
Alex Heeton <alex@heeton.me>
2019-10-30 08:51:33 +0100

    Refactor network module

--- appleyard/lib/appleyard/network.ex
+++ appleyard/lib/appleyard/network.ex
@@ -1,22 +1,14 @@
 defmodule Appleyard.Network do
-  def wifi_stats do
-    with networks <- network().scan("wlan0"),
-         internet <- Enum.find(networks, &(&1[:ssid] == "The Internet")) do
-      extract_stats(internet)
-    end
+  def signal_strength do
+    network_service().scan("wlan0")
+    |> Enum.find(&(&1[:ssid] == "The Internet"))
+    |> extract_strength
   end
 
-  defp extract_stats(nil), do: {:unavailable}
+  defp extract_strength(nil), do: {:error, :unavailable}
+  defp extract_strength(network), do: {:ok, Map.take(network, [:level, :noise, :snr])}
 
-  defp extract_stats(internet) do
-    %{
-      level: internet[:level],
-      noise: internet[:noise],
-      snr: internet[:snr]
-    }
-  end
-
-  defp network do
+  defp network_service do
     Application.get_env(:appleyard, :network_service)
   end
 end
--- appleyard/test/appleyard/network_test.exs
+++ appleyard/test/appleyard/network_test.exs
@@ -2,6 +2,6 @@ defmodule Appleyard.NetworkTest do
   use ExUnit.Case, async: true
 
   test "stats are unavailable when network isn't present" do
-    assert {:unavailable} = Appleyard.Network.wifi_stats()
+    assert {:error, :unavailable} = Appleyard.Network.signal_strength()
   end
 end
